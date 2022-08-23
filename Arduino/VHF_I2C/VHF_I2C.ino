9e5689d41f7b8aba4289ec23d597a4d957624b37
Maciej Swic <maciej@swic.name>
2022-08-23 16:04:25 +0300

    Slave WIP

--- Arduino/VHF_I2C/VHF_I2C.ino
+++ Arduino/VHF_I2C/VHF_I2C.ino
@@ -9,6 +9,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <TM1638plus.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
@@ -22,11 +23,25 @@
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+// TM1638
+#define  STROBE_TM 4 // strobe = GPIO connected to strobe line of module
+#define  CLOCK_TM 6  // clock = GPIO connected to clock line of module
+#define  DIO_TM 7 // data = GPIO connected to data line of module
+bool high_freq = false; //default false,, If using a high freq CPU > ~100 MHZ set to true. 
+
+TM1638plus tm(STROBE_TM, CLOCK_TM , DIO_TM, high_freq);
+
 #define DCSBIOS_IRQ_SERIAL
 #include "DcsBios.h"
 
 void setup() {
   DcsBios::setup();
+
+  tm.displayBegin();
+  tm.displayASCIIwDot(0, '2');
+  tm.displayASCII(1, '3');
+  tm.displayASCII(2, '4');
+  tm.displayASCII(3, '1');
   
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
