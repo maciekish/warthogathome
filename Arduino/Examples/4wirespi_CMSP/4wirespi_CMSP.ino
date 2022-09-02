18a6c2d21400ecf6146b526b8016073f0f613572
Maciej Świć <maciej@swic.name>
2022-09-02 15:06:56 +0300

    Improved CMSP font and drawing

--- Arduino/Examples/4wirespi_CMSP/4wirespi_CMSP.ino
+++ Arduino/Examples/4wirespi_CMSP/4wirespi_CMSP.ino
@@ -9,17 +9,19 @@
 #define U8G2_16BIT
 // U8G2_SSD1322_NHD_256X64_F_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8); // Software SPI with full FB on Arduino Mega.
 // U8G2_SSD1322_NHD_256X64_2_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8); // Hardware SPI on Arduino Nano.
-U8G2_SSD1322_NHD_256X64_2_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 53, /* dc=*/ 49, /* reset=*/ 48); // Hardware SPI on Arduino Mega.
+U8G2_SSD1322_NHD_256X64_F_4W_HW_SPI u8g2(U8G2_R0, /* cs=*/ 53, /* dc=*/ 49, /* reset=*/ 48); // Hardware SPI on Arduino Mega.
 
 // Offsets for slightly randomizing text position.
 int x_offset = 0;
 int y_offset = 0;
 
 // The 2 lines of text to display
-char* cmsp1 = "002 000 1.00 01";
-char* cmsp2 = "CHAF FLAR INTV CYCL";
+char* cmsp1 = "--- --- --- ---    "; // 002 000 1.00 01
+char* cmsp2 = "---- ---- ---- ----"; // CHAF FLAR INTV CYCL
+/*char* cmsp1 = "002 000 1.00 01    ";
+char* cmsp2 = "CHAF FLAR INTV CYCL";*/
 
-unsigned int contrast = 1;
+unsigned int contrast = 8;
 
 #include "CMSP_Large_font.h"
 #include "CMSP_Small_font.h"
@@ -27,28 +29,36 @@ unsigned int contrast = 1;
 void setup() {
   // Slightly randomize position on every boot to avoid burning out the exact same pixels. Afterall, OLEDs are "consumable".
   randomSeed(analogRead(0));
-  x_offset = random(1);
-  y_offset = random(3);
+  // x_offset = random(0); // X randomization disabled, doesn't fit.
+  y_offset = random(8);
   
   u8g2.begin();
-  u8g2.setContrast(1); // Start with lowest brightness in case panel is plugged in and forgotten.
+  u8g2.setContrast(contrast); // Start with lowest brightness in case panel is plugged in and forgotten.
   DcsBios::setup();
 }
 
 void loop() {
   DcsBios::loop();
+  u8g2.clearBuffer();
+  
+  if (contrast > 7) { // My OLED cannot dim a lot, so if brightness is low, just blank the display.
+    // Set the font and draw the top line
+    u8g2.setFont(CMSP_Large_font); // Alternative fonts: u8g2_font_profont29_tf, u8g2_font_inb19_mf
+    u8g2.drawStr(0 + x_offset, 16 + y_offset, cmsp1);
 
-  if (contrast > 0) { // My OLED cannot dim a lot, so if brightness is 0, just blank the display.
-    u8g2.firstPage();
-    do {
-      u8g2.setFont(CMSP_Large_font); // Alternative fonts: u8g2_font_profont29_tf, u8g2_font_inb19_mf
-      u8g2.drawStr(0 + x_offset, 20 + y_offset, cmsp1);
-      u8g2.setFont(CMSP_Small_font);
-      u8g2.drawStr(6 + x_offset, 48 + y_offset, cmsp2);
-    } while ( u8g2.nextPage() );
-  } else {
-    u8g2.clearDisplay();
+    // Split the second line in two be cause the display is 2 pixels too narrow to fit the string properly.
+    char left[9];
+    strncpy(left, cmsp2, 10);
+    char right[10];
+    sprintf(right, "%s", cmsp2+10);
+
+    // Set the font and draw the bottom line in 2 parts
+    u8g2.setFont(CMSP_Small_font);
+    u8g2.drawStr(0 + x_offset, 42 + y_offset, left);
+    u8g2.drawStr(126 + x_offset, 42 + y_offset, right);
   }
+
+  u8g2.sendBuffer();
 }
 
 // Update text line 1
@@ -65,7 +75,7 @@ DcsBios::StringBuffer<19> cmsp2Buffer(0x1014, onCmsp2Change);
 
 // Update brightness
 void onCmspBrtChange(unsigned int newValue) {
-    u8g2.setContrast(newValue / 256.0);
-    contrast = newValue;
+    contrast = newValue / 256.0;
+    u8g2.setContrast(contrast);
 }
 DcsBios::IntegerBuffer cmspBrtBuffer(0x10e6, 0xffff, 0, onCmspBrtChange);
