d84a05590bc2da937b026f94505d7463abe42e1a
Maciej Świć <maciej@swic.name>
2022-09-04 21:29:32 +0300

    Updated TXT Example

--- Arduino/Examples/pararell_tft_display_HX8357/pararell_tft_display_HX8357.ino
+++ Arduino/Examples/pararell_tft_display_HX8357/pararell_tft_display_HX8357.ino
@@ -6,29 +6,30 @@
 TFT_HX8357 tft = TFT_HX8357();
 
 #define text_height 21
+#define text_width 18
 
 // Offsets for slightly randomizing text position to prevent burn-in.
-int x_offset = 13; // Max 27
+int x_offset = 0; // Max 4
 int y_offset = 0; // Max 6
 
 // Text lines to display
 char* lines[] = {
-  " WAYPT         0   D5/B1 ",
-  "                         ",
-  "±1      SP    INIT POSIT¡",
-  "                    UNK  ",
-  "EL:23      ******** DTOT¡",
-  " NO CR                   ",
-  "¡N34°35.367           ?1©",
-  "             WND ***/*** ",
-  "¡E036°00.680         L/L®",
-  "[¶              ]   P1/2 "
+  " WAYPT   F1  1    D5/B1 ",
+  "              FROM:0    ",
+  "\xB1""0           INIT POSIT""\xA1",
+  "                    UNK ",
+  "\xA1""EL:14992 ******** DTOT""\xA1",
+  "                        ",
+  "\xA1""N42""\xB0""17.661          ?6""\xA9",
+  "            WND 272/003  ",
+  "\xA1""E042""\xB0""12.054        L/L""\xAE",
+  "[               ] P1/2  "
 };
 
 void setup() {
   // Slightly randomize position on every boot to avoid burn-in.
   randomSeed(analogRead(0));
-  x_offset = random(15) + 6; // Range 6-21
+  x_offset = random(4);
   y_offset = random(6);
   
   tft.begin();
@@ -56,10 +57,10 @@ int toggle = 0;
 
 void loop() {
   if (toggle == 0) {
-    setCDUText(10, "[               ]   P1/2 ");
+    setCDUText(10, "[               ] P1/2  ");
     toggle = 1;
   } else {
-    setCDUText(10, "[¶              ]   P1/2 ");
+    setCDUText(10, "[""\xB6""              ] P1/2  ");
     toggle = 0;
   }
   
@@ -74,10 +75,16 @@ void setCDUText(int line, char* text) {
 
 // Draw existing CDU Text
 void drawCDULine(int line) {
-  tft.fillRect(x_offset, (y_coordinate_for_line(line) - text_height) + y_offset, 462 - x_offset, text_height, TFT_BLACK);
-  tft.setCursor(x_offset, y_coordinate_for_line(line) + y_offset);
-  tft.print(lines[line - 1]);
-  //tft.drawString(lines[line - 1], x_offset, y_coordinate_for_line(line) + y_offset, 1);
+  // Draw by char to reduce flickering
+  for (int i = 0; i < strlen(lines[line-1]); i++) {
+    int x_pos = (x_offset + (i * text_width)) + i * 2;
+
+    // Clear char
+    tft.fillRect(x_pos, (y_coordinate_for_line(line) - text_height) + y_offset, text_width, text_height, TFT_BLACK);
+
+    // Draw char
+    tft.drawChar(x_pos, y_coordinate_for_line(line) + y_offset, lines[line - 1][i], TFT_GREEN, TFT_BLACK, 3);
+  }
 }
 
 int y_coordinate_for_line(int line) {
