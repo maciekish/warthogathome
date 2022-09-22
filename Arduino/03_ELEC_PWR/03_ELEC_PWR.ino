2d6b108fa2bf26515cffbcb7e85e5b8a1e6f8a54
Maciej Świć <maciej@swic.name>
2022-09-22 12:30:10 +0300

    Code cleanup

--- Arduino/03_ELEC_PWR/03_ELEC_PWR.ino
+++ Arduino/03_ELEC_PWR/03_ELEC_PWR.ino
@@ -1,21 +1,10 @@
-/*
- The following #define tells DCS-BIOS that this is a RS-485 slave device.
- It also sets the address of this slave device. The slave address should be
- between 1 and 126 and must be unique among all devices on the same bus.
-*/
-#define DCSBIOS_RS485_SLAVE 3
-
-/*
- The Arduino pin that is connected to the
- /RE and DE pins on the RS-485 transceiver.
-*/
-#define TXENABLE_PIN 2
-
-// For direct USB Testing
-//#define DCSBIOS_IRQ_SERIAL
+#define DCSBIOS_RS485_SLAVE 3 // The following #define tells DCS-BIOS that this is a RS-485 slave device. It also sets the address of this slave device. The slave address should be between 1 and 126 and must be unique among all devices on the same bus.
+#define TXENABLE_PIN 2 // The Arduino pin that is connected to the /RE and DE pins on the RS-485 transceiver.
+//#define DCSBIOS_IRQ_SERIAL // For direct USB Testing. Only #define euther TXENABLE_PIN or DCSBIOS_IRQ_SERIAL, never both.
 
 #include "DcsBios.h"
 
+// Switches
 DcsBios::Switch2Pos eppApuGenPwr("EPP_APU_GEN_PWR", 6);
 DcsBios::Switch3Pos eppInverter("EPP_INVERTER", 8, 7);
 DcsBios::Switch2Pos eppEmerFlood("EPP_EMER_FLOOD", 9);
@@ -23,6 +12,7 @@ DcsBios::Switch2Pos eppAcGenPwrL("EPP_AC_GEN_PWR_L", 10);
 DcsBios::Switch2Pos eppAcGenPwrR("EPP_AC_GEN_PWR_R", 11);
 DcsBios::Switch2Pos eppBatteryPwr("EPP_BATTERY_PWR", 12);
 
+// Backlight
 void onInternalConsoleLightsChange(unsigned int newValue) {
     analogWrite(5, newValue / 256);
 }
